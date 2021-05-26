750e1b6972aa85b753378a666a89c2ddeb1b3c41
liyupeng01 <liyupeng01@58.com>
2021-05-26 13:55:20 +0800

    add VolatileDemo

---
+++ src/main/java/com/nobug/experiment/juc/VolatileDemo.java
@@ -0,0 +1,39 @@
+package com.nobug.experiment.juc;
+
+/**
+ * @author liyupeng01
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2021年 05月 26日 10:30:00
+ */
+public class VolatileDemo {
+    public static final Object sync = new Object();
+    public static void main(String[] args) {
+        ThreadDemo threadDemo = new ThreadDemo();
+        Thread thread = new Thread(threadDemo);
+        thread.start();
+        while (true) {
+            //System.out.println(threadDemo.flag); sout会读取主内存中的值
+            //Thread.sleep(1);  线程切换会将线程缓存中的值同步到主内存中
+            if (threadDemo.flag) {
+                System.out.println("主线程读取到的flag = " + threadDemo.flag);
+                break;
+            }
+
+        }
+    }
+}
+
+class ThreadDemo implements Runnable {
+    public boolean flag = false;
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        flag = true;
+        System.out.println("ThreadDemo线程修改后的flag = " + flag);
+    }
+}

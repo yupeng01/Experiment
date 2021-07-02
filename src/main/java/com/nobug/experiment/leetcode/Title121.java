ae4f843482d03de8573653c5e530931a4806620e
liyupeng01 <liyupeng01@58.com>
2021-07-02 17:51:33 +0800

    leetcode 136

--- src/main/java/com/nobug/experiment/leetcode/Title121.java
+++ src/main/java/com/nobug/experiment/leetcode/Title121.java
@@ -1,8 +1,33 @@
-package com.nobug.experiment.leetcode;/**
+package com.nobug.experiment.leetcode;
 
-@author liyupeng01
-@version 1.0.0
-@Description TODO
-@createTime 2021年 07月 02日 16:29:00
-*/public class Title121 {
+/**
+ * @author liyupeng01
+ * @version 1.0.0
+ * @Description 输入：[7,1,5,3,6,4]
+ * 输出：5
+ * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+ *
+ * @createTime 2021年 07月 02日 16:29:00
+ */
+public class Title121 {
+    public static void main(String[] args) {
+        int[] numArr = {7, 1, 5, 3, 6, 4};
+        maxProfit(numArr);
+        int[] numArr1 = {7, 6, 4, 3, 1};
+        maxProfit(numArr1);
+    }
+
+    private static void maxProfit(int[] numArr) {
+        int minValue = numArr[0];
+        int maxSub = 0;
+        for (int i = 1; i < numArr.length; i++) {
+            if (numArr[i] < minValue) {
+                minValue = numArr[i];
+                continue;
+            }
+            maxSub = Math.max(numArr[i] - minValue, maxSub);
+        }
+        System.out.println(maxSub);
+    }
 }
--- src/main/java/com/nobug/experiment/leetcode/Title136.java
+++ src/main/java/com/nobug/experiment/leetcode/Title136.java
@@ -1,8 +1,18 @@
-package com.nobug.experiment.leetcode;/**
+package com.nobug.experiment.leetcode;
 
-@author liyupeng01
-@version 1.0.0
-@Description TODO
-@createTime 2021年 07月 02日 17:30:00
-*/public class Title136 {
+/**
+ * @author liyupeng01
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2021年 07月 02日 17:30:00
+ */
+public class Title136 {
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int result = 0;
+        for (int num : arr) {
+            result ^= num;
+        }
+        System.out.println(result);
+    }
 }

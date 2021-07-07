a4ca11a039064504276787499979151a273a0769
liyupeng01 <liyupeng01@58.com>
2021-07-07 11:18:53 +0800

    add leetcode

---
+++ src/main/java/com/nobug/experiment/leetcode/Title141.java
@@ -0,0 +1,8 @@
+package com.nobug.experiment.leetcode;/**
+
+@author liyupeng01
+@version 1.0.0
+@Description TODO
+@createTime 2021年 07月 02日 17:58:00
+*/public class Title141 {
+}
--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `user` VALUES (1, 'zhangsan', '张三', 20, 1.1);
-INSERT INTO `user` VALUES (2, 'shaojie', '少杰', 21, 1.2);
-INSERT INTO `user` VALUES (3, 'zilong', '子龙', 22, 1.3);
-INSERT INTO `user` VALUES (4, 'pengpeng', '鹏鹏', 22, 1.5);
-INSERT INTO `user` VALUES (5, 'tutu', '秃秃', 1, 1.4);

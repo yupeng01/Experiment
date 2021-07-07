c1965c07b89d81898e77acb6d6cb1e70d0e21665
liyupeng01 <liyupeng01@58.com>
2021-07-07 11:59:44 +0800

    test

--- src/main/java/com/nobug/experiment/leetcode/Title141.java
+++ src/main/java/com/nobug/experiment/leetcode/Title141.java
@@ -1,8 +1,53 @@
-package com.nobug.experiment.leetcode;/**
+package com.nobug.experiment.leetcode;
 
-@author liyupeng01
-@version 1.0.0
-@Description TODO
-@createTime 2021年 07月 02日 17:58:00
-*/public class Title141 {
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author liyupeng01
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2021年 07月 02日 17:58:00
+ */
+public class Title141 {
+    public static boolean hasCycle (ListNode head) {
+        ListNode node1 = head;
+        ListNode node2 = head.next;
+        int count = 0;
+        while (node1 != null && node2 != null && node2.next != null && node2.next.next != null) {
+            node1 = node1.next;
+            node2 = node2.next.next;
+            count++;
+            if (node1 == node2) {
+                System.out.println(count);
+                return true;
+            }
+        }
+        System.out.println(count);
+        return false;
+    }
+
+    public static boolean hasCycleV2 (ListNode head) {
+        Set<ListNode> listNodeSet = new HashSet<>();
+        ListNode node = head;
+        while (node != null) {
+            if (!listNodeSet.add(node)) {
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(5);
+        ListNode head1 = new ListNode(1);
+        ListNode head2 = new ListNode(2);
+        ListNode head3 = new ListNode(3);
+        head.next = head1;
+        head1.next = head2;
+        head2.next = head3;
+        head3.next = head1;
+        System.out.println(hasCycleV2(head));
+    }
 }
--- src/main/java/com/nobug/experiment/leetcode/Title160.java
+++ src/main/java/com/nobug/experiment/leetcode/Title160.java
@@ -1,8 +1,48 @@
-package com.nobug.experiment.leetcode;/**
+package com.nobug.experiment.leetcode;
 
-@author liyupeng01
-@version 1.0.0
-@Description TODO
-@createTime 2021年 07月 06日 19:37:00
-*/public class Title160 {
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author liyupeng01
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2021年 07月 06日 19:37:00
+ */
+public class Title160 {
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode node1 = headA;
+        ListNode node2 = headB;
+        Set<ListNode> set = new HashSet();
+        while (node1 != null || node2 != null) {
+            if (node1 != null && !set.add(node1)) {
+                return node1;
+            }
+            if (node2 != null && !set.add(node2)) {
+                return node2;
+            }
+            node1 = node1 != null ? node1.next : null;
+            node2 = node2 != null ? node2.next : null;
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(4);
+        ListNode listNode2 = new ListNode(1);
+        ListNode listNode3 = new ListNode(5);
+        ListNode listNode4 = new ListNode(6);
+        ListNode listNode5 = new ListNode(1);
+        ListNode listNode6 = new ListNode(8);
+        ListNode listNode7 = new ListNode(4);
+        ListNode listNode8 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode6;
+        listNode6.next = listNode7;
+        listNode7.next = listNode8;
+
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+        System.out.println(getIntersectionNode(listNode1, listNode3).val);
+    }
 }

fb4be76f3939f4b03b37b14ee9835b824daeb48f
liyupeng01 <liyupeng01@58.com>
2021-09-07 10:37:22 +0800

    add new leetcode

--- src/main/java/com/nobug/experiment/leetcode/Title1221.java
+++ src/main/java/com/nobug/experiment/leetcode/Title1221.java
@@ -1,2 +1,28 @@
-package com.nobug.experiment.leetcode;public class Title1221 {
+package com.nobug.experiment.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Title1221 {
+    public static int balancedStringSplit(String s) {
+        Deque<Character> stack = new LinkedList();
+        char[] charArr = s.toCharArray();
+        int result = 0;
+        for (char c : charArr) {
+            if (stack.isEmpty()) {
+                result++;
+                stack.push(c);
+                continue;
+            }
+            if (stack.peek() == c) {
+                stack.push(c);
+            } else {
+                stack.pop();
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        balancedStringSplit("RLRRLLRLRL");
+    }
 }
